1911c2e9971e4186cfa21ebda8cf21735147d22d
Claudine Pradal <claudinecartailler@gmail.com>
2018-03-24 20:56:30 +0100

    Change due to Volchan update

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,7 +1,5 @@
 class Mission < ApplicationRecord
 
-  belongs_to :user
-  belongs_to :coordinator, class_name: 'User', foreign_key: 'user_id' #, inverse_of: :resources
   has_many :user_missions, dependent: :destroy
   has_many :mission_resources, dependent: :nullify
   has_many :resources, through: :mission_resources
