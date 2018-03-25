4eb790f332784e39dfd9b5a35e890276d9345d55
Maxime Boue <abriko@yahoo.fr>
2018-03-25 10:16:19 +0200

    Add missions loops and condition.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,7 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :contact]
 
-  def home
-  end
+  def home; end
 
-  def contact
-  end
+  def contact; end
 end
