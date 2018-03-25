8c96bda5ca80d8d1dba142892db3c7fd9f61349b
Maxime Boue <abriko@yahoo.fr>
2018-03-25 18:52:12 +0200

    Front coordinator show, link is DEAD because route doesn't exist yet

--- app/controllers/coordinator/users_controller.rb
+++ app/controllers/coordinator/users_controller.rb
@@ -1,5 +1,10 @@
 module Coordinator
   class UsersController < ApplicationController
-    def show; end
+    def show;
+      @user = User.find(params[:id])
+      @ambassadors = User.where(coordinator: @user).first(5)
+      @ambassador_missions = @ambassadors.map{ |ambassador| ambassador.user_missions.booked }.flatten.first(5)
+      @missions_todo = @user.missions.first(4)
+    end
   end
 end
