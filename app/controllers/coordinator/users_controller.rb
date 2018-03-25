a302b454390974a044073d67f512ae08dab28524
ClaudineP <34744530+ClaudineP435433@users.noreply.github.com>
2018-03-25 19:07:31 +0200

    Update users_controller.rb

--- app/controllers/coordinator/users_controller.rb
+++ app/controllers/coordinator/users_controller.rb
@@ -1,6 +1,6 @@
 module Coordinator
   class UsersController < ApplicationController
-    def show;
+    def show
       @user = User.find(params[:id])
       @ambassadors = User.where(coordinator: @user).first(5)
       @ambassador_missions = @ambassadors.map{ |ambassador| ambassador.user_missions.booked }.flatten.first(5)
